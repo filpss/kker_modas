399944d12fdfc63b8bb9180eb6e91c5ec04ae5d5
filps <filipepinho2010@hotmail.com>
2024-07-31 21:52:10 -0300

    Atualizando a estrutura de pastas do projeto, criando base do roteamento e controlando acesso às páginas.

--- public/index.php
+++ public/index.php
@@ -1,8 +1,38 @@
 <?php
 
-require_once 'config.php';
-
-include_once BASE_PATH . '/src/views/header.php';
-
-// Iniciando sessão
 session_start();
+//$_SESSION['user'] = 'filipe';
+//session_destroy();
+
+$allowed_routes = require_once __DIR__ . '/../config/router/router.php';
+
+$route = $_GET['route'] ?? 'home';
+
+if(!isset($_SESSION['user'])){
+    $route = 'login';
+}
+
+if(isset($_SESSION['user']) && $route === 'login'){
+    $route = 'home';
+}
+
+if(!in_array($route, $allowed_routes)){
+    $route = '404';
+}
+
+$page = null;
+switch($route){
+    case '404':
+        $page = '404.php';
+        break;
+    case 'home':
+        $page = 'home.php';
+        break;
+    case 'login':
+        $page = 'login_page.php';
+        break;
+}
+
+require_once __DIR__ . '/../views/head.php';
+require_once __DIR__ . "/../views/pages/$page";
+require_once __DIR__ . '/../views/footer.php';
