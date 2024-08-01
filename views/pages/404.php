feff7f3ded5ce1afb0316bd3ad9a805d6455f381
filps <filipepinho2010@hotmail.com>
2024-07-31 21:53:56 -0300

    Atualizando a estrutura de pastas do projeto, criando base do roteamento e controlando acesso às páginas.

---
+++ views/pages/404.php
@@ -0,0 +1,3 @@
+<?php
+
+echo '<h1>Página não encontrada</h1>';
---
+++ views/pages/login_page.php
@@ -0,0 +1,29 @@
+    <?php
+    include_once __DIR__ . '/../head.php';
+    ?>
+
+<link rel="stylesheet" href="../assets/css/login_page.css">
+<section class="form-section">
+    <div class="form">
+        <div class="logo-txt">
+            <img class="img-login-page" src="../assets/img/mini-logo-kker.svg" alt="Logomarca da empresa KKER">
+            <p>KK&R</p>
+        </div>
+
+        <h2>Faça Login</h2>
+        <form method="get" action="#">
+            <label for="login">Login</label>
+            <input placeholder="Digite seu usuário" type="text" id="login"/>
+            <label for="password">Senha</label>
+            <input placeholder="******" type="password" id="password"/>
+            <div class="btn">
+                <button>Enviar</button>
+            </div>
+            <p class="forgot-password">Esqueceu a senha? <a>Clique aqui!</a></p>
+        </form>
+    </div>
+
+    <div class="div-img">
+        <img src="../assets/img/logo_kker.svg" alt="Foto grande com a logo">
+    </div>
+</section>
